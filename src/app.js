5789754e20669c8cbb8b1f820e04378522e35e61
Draxeddon <carlos_18@live.com.mx>
2026-05-31 20:13:29 -0600

    Primer Commit

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type'],
     credentials:true,
 }));
-app.options('*', cors());
+app.options('(.*)', cors());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
